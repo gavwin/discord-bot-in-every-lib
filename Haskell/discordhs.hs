dc2c9bb9f494e428cfcccd661e4810cf4263ee21
Gavin Wainwright <gav.wainwright@gmail.com>
2017-04-14 02:13:13 -0400

    Create discordhs.hs

---
+++ Haskell/discordhs.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
+import Data.Text
+import Pipes
+
+import Network.Discord
+
+reply :: Message -> Text -> Effect DiscordM ()
+reply Message{messageChannel=chan} cont = fetch' $ CreateMessage chan cont Nothing
+
+main :: IO ()
+main = runBot (Bot "TOKEN") $ do
+  with ReadyEvent $ \(Init v u _ _ _) ->
+    liftIO . putStrLn $ "Connected to gateway v" ++ show v ++ " as user " ++ show u
+
+  with MessageCreateEvent $ \msg@Message{..} -> do
+    when ("ping" `isPrefixOf` messageContent && (not . userIsBot $ messageAuthor)) $
+      reply msg "Pong!"
