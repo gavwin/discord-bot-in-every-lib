de32914ace48abc8d2fb3e6caeffe03075844b33
Gavin Wainwright <gav.wainwright@gmail.com>
2017-04-14 19:34:57 -0400

    Update discordhs.hs

--- Haskell/discordhs.hs
+++ Haskell/discordhs.hs
@@ -8,7 +8,7 @@ reply :: Message -> Text -> Effect DiscordM ()
 reply Message{messageChannel=chan} cont = fetch' $ CreateMessage chan cont Nothing
 
 main :: IO ()
-main = runBot (Bot "TOKEN") $ do
+main = runBot (Bot "token") $ do
   with ReadyEvent $ \(Init v u _ _ _) ->
     liftIO . putStrLn $ "Connected to gateway v" ++ show v ++ " as user " ++ show u
 
